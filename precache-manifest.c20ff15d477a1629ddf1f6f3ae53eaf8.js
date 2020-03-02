7fbbddf3efde9cc333e81a3b63b18ab5fbd2bd76
notoya <atoropos666@gmail.com>
2020-03-02 16:45:13 +0900

    Updates

---
+++ precache-manifest.c20ff15d477a1629ddf1f6f3ae53eaf8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fff9d66736a581e2a61e7f8bad58f9e5",
+    "url": "/react-smashBros/index.html"
+  },
+  {
+    "revision": "918a24e31d1cfac519d2",
+    "url": "/react-smashBros/static/js/2.dae9cc34.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-smashBros/static/js/2.dae9cc34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40449c0c3c3a7fe6c04b",
+    "url": "/react-smashBros/static/js/main.017a7042.chunk.js"
+  },
+  {
+    "revision": "3a82cadff3a50c7afb28",
+    "url": "/react-smashBros/static/js/runtime-main.0340f0cb.js"
+  }
+]);
