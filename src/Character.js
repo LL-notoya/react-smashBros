19518db44a105b570edb8f7f2c36c4adc92168aa
notoya <atoropos666@gmail.com>
2020-03-02 19:32:46 +0900

    indexからidに変更

--- src/Character.js
+++ src/Character.js
@@ -4,24 +4,20 @@ import React, { useState } from "react"
 const Character = () => {
 
     const player = [
-        { name: "スネーク", skill: "射撃" },
-        { name: "ピカチュウ", skill: "でんこうせっか" },
-        { name: "ロックマン", skill: "ロックバスター" },
-        { name: "テリー", skill: "パワーゲイザー" },
-        { name: "リュウ", skill: "昇竜拳" }
+        {id: 1, name: "スネーク", skill: "射撃" },
+        {id: 2, name: "ピカチュウ", skill: "でんこうせっか" },
+        {id: 3, name: "ロックマン", skill: "ロックバスター" },
+        {id: 4, name: "テリー", skill: "パワーゲイザー" },
+        {id: 5, name: "リュウ", skill: "昇竜拳" }
     ]
 
     return (
-        // <>
-        // {
-            player.map((data, index) =>
+            player.map((data, id) =>
                 <Profile
                     data={data}
-                    key={index}
+                    key={id}
                 />
-            )
-            // }
-        // </>
+                )
     )
 }
 
